f31bddfb6449a7331031e2fe7c16d2d4d00171d1
Vuk Tacic <vuk.tacic@gmail.com>
2026-05-26 22:57:01 -0700

    Update data pipeline
    Move average calculations and rankings to the data pipeline. Reduces on-the-fly calculations and prevents the loading of 750 json files for the rankings.

--- app/components/RankingList.tsx
+++ app/components/RankingList.tsx
@@ -1,20 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
-type SchoolIndex = {
-    SCHOOL_NUMBER: string;
-    SCHOOL_NAME: string;
-    DISTRICT_NUMBER: string;
-    DISTRICT_NAME: string;
-
-    LOCATION: {
-        lat: string;
-        lng: string;
-    }
-}
-
-export default function RankingList({title, data = []}: {title: string, data: any[]}) {
+export default function RankingList({title, data = []}: {title: string, data: any[] | undefined}) {
     return (
         <div className="h-[19vh] bg-white z-1000 p-3 rounded shadow text-sm text-black mb-4">
 
@@ -22,7 +8,7 @@ export default function RankingList({title, data = []}: {title: string, data: an
 
             <div className="h-[15vh] overflow-y-auto p-4">
                 {
-                    data.map((school: { schoolname: string, schoolnumber: string, avg: number, writers: number }, index: number) => (
+                    data?.map((school: { schoolname: string, schoolnumber: string, avg: number, writers: number }, index: number) => (
                         <div key={school.schoolnumber}>
                             {index + 1}. {school.schoolname} ({school.schoolnumber}) - {school.avg.toFixed(2)} - {school.writers} Exams
                         </div>
