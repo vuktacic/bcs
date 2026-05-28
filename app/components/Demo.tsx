2b7e5e4d450e3d1abe2c97678c683e9e81fcf1c7
Vuk Tacic <vuk.tacic@gmail.com>
2026-05-28 15:36:32 -0700

    Fixed mobile panel scrolling & layout

--- app/components/Demo.tsx
+++ app/components/Demo.tsx
@@ -1,7 +1,7 @@
 export default function Demo() {
   return (
     <div className="absolute top-4 left-1/2 -translate-x-1/2 z-1000 text-sm text-red-500">
-      <h1 className="text-4xl font-bold mb-4">Demo</h1>
+      <h1 className="text-2xl md:text-4xl font-bold mb-4">Demo</h1>
     </div>
   );
 }
--- app/components/RankingList.tsx
+++ app/components/RankingList.tsx
@@ -2,7 +2,7 @@
 
 export default function RankingList({ title, data = [] }: { title: string, data: any[] | undefined }) {
   return (
-    <div className="p-3 bg-white z-1000 text-sm text-black">
+    <div className="p-3 bg-white z-1000 text-2xs md:text-xs text-black overflow-x-hidden truncate mr-3">
       {
         data?.map((school: { schoolname: string, schoolnumber: string, avg: number, writers: number }, index: number) => (
           <div key={school.schoolnumber}>
