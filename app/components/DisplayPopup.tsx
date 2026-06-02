ab3b6175d5e9c76cd6986cbc75904a52cfd01b6f
Vuk Tacic <vuk.tacic@gmail.com>
2026-06-02 11:24:46 -0700

    Change linechart legend formatting to format nicely on desktop & mobile

--- app/components/DisplayPopup.tsx
+++ app/components/DisplayPopup.tsx
@@ -1,4 +1,6 @@
-import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, TooltipContentProps, XAxis, YAxis } from "recharts";
+"use client";
+
+import { CartesianGrid, Legend, LegendProps, Line, LineChart, ResponsiveContainer, Tooltip, TooltipContentProps, XAxis, YAxis } from "recharts";
 
 const na10 = "Numeracy Assessment 10";
 const la10 = "Literacy Assessment 10";
@@ -48,8 +50,8 @@ const CustomTooltip = ({ active, payload, label }: TooltipContentProps) => {
                     <div className="" style={{ color: entry.color }}>
                       {entry.value}%
                     </div>
-                    <div className="text-xs" style={{ color: prov.color }}>
-                      {prov.value}%
+                    <div className="text-xs" style={{ color: prov?.color }}>
+                      {prov?.value}%
                     </div>
                   </div>
                 );
@@ -62,9 +64,29 @@ const CustomTooltip = ({ active, payload, label }: TooltipContentProps) => {
   );
 };
 
+const CustomLegend = (props: LegendProps) => {
+  const { payload } = props;
+  return (
+    <div className="custom-legend grid grid-cols-2 grid-rows-3">
+      {[na10, la10, la12].map((assessment) => {
+        const entry = payload?.find((p: any) => p.dataKey === assessment);
+        const prov = payload?.find((p: any) => p.dataKey === `${assessment}_prov`);
+        return (
+          <div key={assessment} className="col-span-2 row-span-1 flex flex-row gap-2 items-center">
+            <div className="text-xs text-right w-28" style={{ color: entry?.color ?? undefined }} title={String(entry?.value ?? assessment)}>
+              {String(entry?.value ?? assessment).replace("Assessment ", "")}
+            </div>
+            <div className="text-xs text-left w-32" style={{ color: prov?.color ?? undefined }} title={String(prov?.value ?? "")}>{String(prov?.value ?? "").replace("Assessment ", "")}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
 export default function DisplayPopup({ selected, object, isSchool, provinceData, popupWidth }: { selected: any, object: any, isSchool: boolean, provinceData: any, popupWidth: number }) {
   return (
-    <div style={{ width: `${popupWidth}px`, maxWidth: `${popupWidth}px` }} className="bg-background text-foreground">
+    <div className="bg-background text-foreground w-full h-full">
       {isSchool ?
         <div>
           <strong style={{ color: object.PUBLIC ? "var(--color-public-light)" : "var(--color-independent-light)" }}>
@@ -105,18 +127,19 @@ export default function DisplayPopup({ selected, object, isSchool, provinceData,
               content={CustomTooltip}
               position={{ x: 70, y: 5 }}
             />
-            <Legend align="left" verticalAlign="bottom" layout="horizontal" labelStyle={{ textAlign: "left" }} wrapperStyle={{
-              paddingTop: "20px",
+            <Legend content={CustomLegend} wrapperStyle={{
               display: "flex",
               flexDirection: "column",
+              alignItems: "center",
               gap: "4px"
             }} />
-            <Line connectNulls type="monotone" name={`${na10.replace("Assessment ", "")}         `} dataKey={na10} stroke="#a09cff" strokeWidth={2} />
-            <Line connectNulls type="monotone" name={`${la10.replace("Assessment ", "")}         `} dataKey={la10} stroke="#7ff0a9" strokeWidth={2} />
-            <Line connectNulls type="monotone" name={`${la12.replace("Assessment ", "")}         `} dataKey={la12} stroke="#ffc658" strokeWidth={2} />
-            <Line connectNulls strokeDasharray="5 5" type="monotone" name={`${na10.replace("Assessment ", "")}_prov        `} dataKey={`${na10}_prov`} stroke="#4e4b82" strokeWidth={2} />
-            <Line connectNulls strokeDasharray="5 5" type="monotone" name={`${la10.replace("Assessment ", "")}_prov        `} dataKey={`${la10}_prov`} stroke="#4f7d5f" strokeWidth={2} />
-            <Line connectNulls strokeDasharray="5 5" type="monotone" name={`${la12.replace("Assessment ", "")}_prov        `} dataKey={`${la12}_prov`} stroke="#baba6e" strokeWidth={2} />
+            {/* only add padders if viewport below md */}
+            <Line connectNulls type="monotone" name={na10} dataKey={na10} stroke="#a09cff" strokeWidth={2} />
+            <Line connectNulls type="monotone" name={la10} dataKey={la10} stroke="#7ff0a9" strokeWidth={2} />
+            <Line connectNulls type="monotone" name={la12} dataKey={la12} stroke="#ffc658" strokeWidth={2} />
+            <Line connectNulls strokeDasharray="5 5" type="monotone" name={`${na10}_prov`} dataKey={`${na10}_prov`} stroke="#4e4b82" strokeWidth={2} />
+            <Line connectNulls strokeDasharray="5 5" type="monotone" name={`${la10}_prov`} dataKey={`${la10}_prov`} stroke="#4f7d5f" strokeWidth={2} />
+            <Line connectNulls strokeDasharray="5 5" type="monotone" name={`${la12}_prov`} dataKey={`${la12}_prov`} stroke="#baba6e" strokeWidth={2} />
           </LineChart>
         </ResponsiveContainer>
       ) : (
